b7b96b0fc1d944232a581b720b162f01679c0700
onry14 <onrywharton14@gmail.com>
2025-08-03 12:18:25 +0100

    Add files via upload

---
+++ MovieLeague.jsx
@@ -0,0 +1,1 @@
+# Placeholder for the full MovieLeague component (already built earlier)
---
+++ main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import MovieLeague from "./MovieLeague";
+import "./style.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <MovieLeague />
+  </React.StrictMode>
+);
